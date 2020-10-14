dfdada9adbb366b137ca709bb48c8364d58d87a6
Amy Gamble <amymgambl@gmail.com>
2020-10-13 19:53:10 -0500

    add original project

---
+++ RestaurantMenu/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantMenu
+{
+    class Menu
+    {
+        private DateTime lastUpdated;
+        private Dictionary<string, MenuItem> menuList; 
+
+        public Menu()
+        {
+
+        }
+    }
+}
---
+++ RestaurantMenu/MenuItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantMenu
+{
+    class MenuItem
+    {
+        //private string name;
+        private float price;
+        private string description;
+        private string category;
+        private DateTime itemAdded;
+
+        public float Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public DateTime ItemAdded { get; }
+        public MenuItem(float price, string description, string category, DateTime itemAdded)
+        {
+            this.price = price;
+            this.description = description;
+            this.category = category;
+            this.itemAdded = itemAdded;
+        }
+    }
+}
